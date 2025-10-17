cc9119e655048b37458e0c6273e16b176b5bece8
Ezequiel Mensur <ezequiel.mensur@oda.com>
2025-10-16 22:11:35 -0300

    refactor(routes): JWT cookie + auth:api guard and v1 grouping

--- routes/api.php
+++ routes/api.php
@@ -18,14 +18,12 @@ Route::prefix('v1')->group(function (): void {
         Route::get('users', [UserController::class, 'index']);
         Route::get('users/{id}', [UserController::class, 'show']);
         Route::put('users/{id}', [UserController::class, 'update']);
-        Route::patch('users/{id}', [UserController::class, 'update']);
         Route::delete('users/{id}', [UserController::class, 'destroy']);
         Route::get('me', [AuthController::class, 'me']);
         Route::get('posts', [PostController::class, 'index']);
         Route::get('posts/{id}', [PostController::class, 'show']);
         Route::post('posts', [PostController::class, 'store']);
         Route::put('posts/{id}', [PostController::class, 'update']);
-        Route::patch('posts/{id}', [PostController::class, 'update']);
         Route::delete('posts/{id}', [PostController::class, 'destroy']);
         Route::post('posts/{id}/restore', [PostController::class, 'restore']);
     });
